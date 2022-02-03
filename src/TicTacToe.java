9cb5e67f69b1cf962f0b3c0aafee12363cabc221
abhirimal <abhirimal009@gmail.com>
2022-02-04 03:44:30 +0545

    player cpu created

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -10,44 +11,20 @@ public class TicTacToe {
 
         printGameBoard(gameBoard);
 
-        System.out.println("Enter your position between (0-9)");
-        Scanner sc = new Scanner(System.in);
-        int pos = sc.nextInt();
-        System.out.println(pos);
+        while(true){
+            System.out.println("Enter your position between (0-9)");
+            Scanner sc = new Scanner(System.in);
+            int playerPos = sc.nextInt();
+            System.out.println(playerPos);
+            placePiece(gameBoard,playerPos, "player");
 
-        switch(pos) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-            case 3:
-                gameBoard[0][5] = 'X';
-                break;
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-            case 6:
-                gameBoard[2][5] = 'X';
-                break;
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-            case 9:
-                gameBoard[4][6] = 'X';
-                break;
-            default:
-                break;
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9)  + 1;
+            placePiece(gameBoard,cpuPos, "cpu");
 
+            printGameBoard(gameBoard);
         }
-        printGameBoard(gameBoard);
+
     }
 
     public static void printGameBoard(char[][] gameBoard){
@@ -56,6 +33,47 @@ public class TicTacToe {
                 System.out.print(gameBoard[i][j] + "");
             }
             System.out.print("\n");
+        }
+    }
+    public static void placePiece(char[][] gameBoard, int pos, String user){
+        char symbol='\u00A0';
+        if(user.equals("player")){
+            symbol='X';
+        }
+        else if(user.equals("cpu")){
+            symbol='O';
+        }
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+            default:
+                break;
+
         }
     }
 }
